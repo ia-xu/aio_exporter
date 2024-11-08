44cc77d007c38b8514757a325e0011e8ca37121b
anxu829 <anxu829@gmail.com>
2024-11-08 13:19:22 +0800

    添加了 workers 参数，避免一个进程没有办法同时处理多个定时任务

--- aio_exporter/cli/main.py
+++ aio_exporter/cli/main.py
@@ -7,4 +7,10 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("APP_PORT", 31006))
-    uvicorn.run("aio_exporter.cli.app.main:app", host="0.0.0.0", port=port)
+    uvicorn.run(
+        "aio_exporter.cli.app.main:app",
+        host="0.0.0.0",
+        port=port,
+        workers=3
+    )
+
