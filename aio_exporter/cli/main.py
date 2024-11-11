35ddf026f54c9120e01f1e4b679df27f17b261a5
anxu829 <anxu829@gmail.com>
2024-11-11 19:08:09 +0800

    添加了worker的启动参数

--- aio_exporter/cli/main.py
+++ aio_exporter/cli/main.py
@@ -2,15 +2,36 @@ import os
 
 import requests
 import uvicorn
+import argparse
 
-# copy from tangruyi
 
 if __name__ == "__main__":
-    port = int(os.environ.get("APP_PORT", 31006))
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description="Run the AIO Exporter application.")
+
+    # Add an argument for the port number
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=int(os.environ.get("APP_PORT", 31006)),
+        help="Port number to run the application on. Defaults to APP_PORT environment variable or 31006 if not set."
+    )
+    # Add an argument for the number of workers
+    parser.add_argument(
+        "--workers",
+        type=int,
+        default=int(os.environ.get("APP_WORKERS", 3)),
+        help="Number of workers to run the application with. Defaults to APP_WORKERS environment variable or 3 if not set."
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # Run the application with the specified port
     uvicorn.run(
         "aio_exporter.cli.app.main:app",
         host="0.0.0.0",
-        port=port,
-        workers=3
+        port=args.port,
+        workers=args.workers
     )
 
